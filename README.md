bee015c576867e621adaaedfb2e705cd23083d4c
Leon <tahjib.leon399@gmail.com>
2023-06-05 22:26:52 +0600

    https request response

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Environment setup
 ## Express server setup
 ## HTTP request & response
+### HTTP methods - GET, POST, PUT, DELETE
+### HTTP status code - 200, 404, 500, 400 
--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get("/test", (request, response)=>{
+response.status(200).send({
+    message: 'Server is running ok',
+});
+});
+
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');

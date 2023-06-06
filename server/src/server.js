351588697cf034669d9a69863fa3ae567c01a934
Leon <tahjib.leon399@gmail.com>
2023-06-06 07:25:30 +0600

    middleware basic

--- server/src/server.js
+++ server/src/server.js
@@ -4,11 +4,33 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
-app.get("/test", (request, response)=>{
-response.status(200).send({
-    message: 'Server on port 3001 is running ok',    
+const isLoggedIn = (req, res, next) => {
+    const login = true;
+    if(login){
+        req.body.id = 101;
+        next();
+    }else{
+        return res.status(401).json({message: 'Please login first'});
+    }
+    
+}
+// works for every request
+// app.use(isLoggedIn);
+
+app.get("/test", (request, response) => {
+    response.status(200).send({
+        message: 'Server on port 3001 is running ok',
+    });
 });
+
+app.get("/api/user", isLoggedIn, (request, response) => {
+    console.log(request.body.id);
+    response.status(200).send({
+        message: 'User profile is running ok',
+    });
 });
 
 

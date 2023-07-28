e8843bf9bf71725b323d8791decc1dd0c17608de
Leon <tahjib.leon399@gmail.com>
2023-07-28 20:06:26 +0600

    error handling middleware

--- server/src/server.js
+++ server/src/server.js
@@ -4,36 +4,51 @@ const morgan = require('morgan');
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 //! middleware
 const isLoggedIn = (req, res, next) => {
     const logIn = true;
-    if(logIn){
+    if (logIn) {
         req.body.id = 101;
         next();
-    }else{
+    } else {
         return res.status(401).json({
             message: 'Please log in first'
         })
     }
-//  console.log("isLoggedIn middleware");
+    //  console.log("isLoggedIn middleware");
 };
 
 //? used isLoggedIn middleware for this get request
-app.get("/api/user", isLoggedIn ,(req, res) => {
+app.get("/api/user", isLoggedIn, (req, res) => {
     console.log(req.body.id);
     // console.log('User profile');
     res.status(200).send({
         message: "User profile is returned"
     });
 });
-
+// get request
 app.get("/products", (req, res) => {
     res.status(200).send({
         message: "Prodcuts are returned"
     });
 });
 
+
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
 });
+
+// client error handling 
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Route not found'
+    })
+    next();
+})
+
+// server error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
